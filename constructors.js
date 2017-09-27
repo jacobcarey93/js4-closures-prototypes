57bb895ee2f6f000087b5e24d8bbef4467e6683d
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-09-27 16:32:09 -0600

    completed constructor functions portion

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@
 
 // Code here
 
-
+function CarFactory(make, model){
+  this.make =  make;
+  this.model = model
+}
 
 
 
@@ -28,7 +31,7 @@ function Employee(name, email, hireDate) {
 // Code here
 
 
-
+var bob = new Employee("Bob", "bob@gmail.com", "01-02-98")
 
 
 
@@ -42,6 +45,15 @@ function Employee(name, email, hireDate) {
 
 // Code here
 
+function Car(make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0
+  this.moveCar = function(){
+    return this.move += 10;
+  }
+}
 
 
 
